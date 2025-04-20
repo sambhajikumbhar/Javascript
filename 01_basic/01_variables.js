b57914d9dec5e983a193b5b0a2c867b0c5258989
sambhaji <kumbharsambhaji156@gmail.com>
2025-04-20 15:18:42 +0530

    basic 2

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const acountId = 12345
+let acoountEmail = "sam@gmail.com"
+var accountPassword ="09090"
+
+acoountEmail ="hc@gmail.com"
+accountPassword ="21212";
+
+let accountState ;
+console.log(acountId);
+
+/* 
+var is block scope and function scope
+
+
+*/
+ console.table([acountId,acoountEmail,accountPassword,accountState]);
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+//number
+//string
+//bigInt
+//boolean 
+// null standalone value 
+//undefined
+//symbol -> unique
+
+
+//object
+
+let str = "sam";
+
+console.log(typeof str);
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
